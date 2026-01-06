a69276c3553ea6522766cf684cb988e2a1c433df
KSharma-SourceCode <karan.bmit.2023@gmail.com>
2026-01-06 20:35:26 +0530

    Updated Screenshot & Metrics

--- M-Tech. Semester 3/MLOPS/Assignment-1/heart-disease-mlops/src/app.py
+++ M-Tech. Semester 3/MLOPS/Assignment-1/heart-disease-mlops/src/app.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
+from fastapi import Response
+import time
 import joblib,os
 import pandas as pd
 
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total HTTP requests",
+    ["method", "endpoint", "status"]
+)
+
+REQUEST_LATENCY = Histogram(
+    "http_request_latency_seconds",
+    "Request latency",
+    ["endpoint"]
+)
+
 app = FastAPI(title="Heart Disease Prediction API")
+
+@app.middleware("http")
+async def prometheus_middleware(request, call_next):
+    start = time.time()
+    response = await call_next(request)
+    latency = time.time() - start
+
+    REQUEST_COUNT.labels(
+        request.method,
+        request.url.path,
+        response.status_code
+    ).inc()
+
+    REQUEST_LATENCY.labels(
+        request.url.path
+    ).observe(latency)
+
+    return response
 
 # -------------------------
 # Model loading
@@ -59,6 +92,14 @@ class PatientData(BaseModel):
 @app.get("/")
 def health_check():
     return {"status": "API is running"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "API is running"}
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
 
 @app.post("/predict")
 def predict(data: PatientData):
